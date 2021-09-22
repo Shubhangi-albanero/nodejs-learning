e0525f0d2c8f16481e965a96d48a1aa8809fbdbe
Shubhangi Gupta <shubhangihu@gmail.com>
2021-09-22 18:02:34 +0530

    pink

--- taskAPIs/routes/index.js
+++ taskAPIs/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const mongodb =require("mongodb");
+const mongodb = require("mongodb");
 const { MongoClient } = require("mongodb");
 
 let db
@@ -16,101 +16,148 @@ MongoClient.connect(
   }
 )
 
-router.get('/', function(req, res, next) {
-  db.collection('users')
-  .find()
-  .toArray(function (err, items) {
-    res.json(items)
-  })
-});
-
-router.get('/:id', function(req, res, next) {
-  const { id } = req.params
-  db.collection('users')
-  .find({
-    _id:new mongodb.ObjectId(id)
-  })
-  .toArray(function (err, items) {
-    res.json(items)
-  })
-});
-
-router.post('/', function(req, res, next) {
-  const {firstName, lastName, age}=req.body;
-  let user={
-    firstName:firstName,
-    lastName:lastName,
-    age:age
-  }
-  db.collection('users').insertOne(user, function (
-    err,
-    info
-  ) {
-    res.json(info)
-  })
-});
-
-router.patch('/:id', function(req, res, next) {
-  const {firstName, lastName, age}=req.body;
-  const { id } = req.params
-  let user={
-    firstName:firstName,
-    lastName:lastName,
-    age:age
-  }
-  db.collection('users').findOneAndUpdate(
-    { _id: new mongodb.ObjectId(id) },
-    { $set: user },
-    function () {
-      
-      res.json({
-        message:"Users Updated succesfully"
-      });
-    }
-  )
-});
-
-
-router.delete('/:id', function(req, res, next) {
-  const { id } = req.params
-  db.collection('users').deleteOne(
-    { _id: new mongodb.ObjectId(id) },
-    function () {
-      res.json({
-        message:"Users deleted succesfully"
-      });
-    }
-  )
-
-});
-
-router.post('/follow/:id', function(req, res, next) {
-  const {user_id}=req.body;
-  const { id } = req.params
-  let data={
-    user_id:user_id,
-    follower_id:id
+router.get('/', async (req, res, next) => {
+  try {
+    let users = await db.collection('users').find().toArray()
+    res.json(users);
 
   }
-  db.collection('followers').insertOne(data,
-    
-    function () {
-      
-      res.json({
-        message:"fOLLOWERS LIST Updated succesfully"
-      });
-    }
-  )
+  catch (error) {
+    res.status(400).json({
+      message: "Something went wrong"
+    })
+  }
 });
 
-router.get('/followers/:id', function(req, res, next) {
-  const { id } = req.params
-  db.collection('followers')
-  .find({
-    user_id:id
-  })
-  .toArray(function (err, items) {
-    res.json(items)
-  })
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    let users = await db.collection('users')
+      .find({
+        _id: new mongodb.ObjectId(id)
+      })
+      .toArray()
+
+    res.json(users)
+  }
+  catch (error) {
+    res.status(400).json({
+      message: "Something went wrong"
+    })
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const { firstName, lastName, age } = req.body;
+    let user = {
+      firstName: firstName,
+      lastName: lastName,
+      age: age
+    }
+    let { ops: users } = await db.collection('users').insertOne(user)
+    console.log(users);
+    res.json({
+      message: "uSEr is CreATed SuccesFuLLy"
+    })
+  }
+  catch (error) {
+    res.status(400).json({
+      message: "Something went wrong"
+    })
+  }
+});
+
+router.patch('/:id', async (req, res, next) => {
+  try {
+    const { firstName, lastName, age } = req.body;
+    const { id } = req.params
+    let user = {
+      firstName: firstName,
+      lastName: lastName,
+      age: age
+    }
+    let users = await db.collection('users').findOneAndUpdate(
+      { _id: new mongodb.ObjectId(id) },
+      { $set: user },
+      function () {
+
+        res.json({
+          message: "Users Updated succesfully"
+        });
+      }
+    )
+  }
+  catch (error) {
+    res.status(400).json({
+      message: "Something went wrong"
+    })
+  }
+});
+
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    let users = await db.collection('users').deleteOne(
+      { _id: new mongodb.ObjectId(id) },
+      function () {
+        res.json({
+          message: "Users deleted succesfully"
+        });
+      }
+    )
+  }
+  catch (error) {
+
+    res.status(400).json({
+      message: "Something went wrong"
+    })
+
+  }
+
+});
+
+router.post('/follow/:id', async (req, res, next) => {
+  try {
+    const { user_id } = req.body;
+    const { id } = req.params
+    let data = {
+      user_id: user_id,
+      follower_id: id
+
+    }
+    await db.collection('followers').insertOne(data,
+
+      function () {
+
+        res.json({
+          message: "fOLLOWERS LIST Updated succesfully"
+        });
+      }
+    )
+  }
+  catch (error) {
+    res.status(400).json({
+      message: "Something went wrong"
+    })
+  }
+});
+
+router.get('/followers/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    let followers = await db.collection('followers')
+      .find({
+        user_id: id
+      })
+      .toArray()
+    res.json(followers);
+  }
+  catch (error) {
+    res.status(400).json({
+      message: "Something went wrong"
+    })
+  }
 });
 module.exports = router;
